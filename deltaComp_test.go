ce21fef2f9b8c11b8131ae4b5164fbe612f7c7d8
abhinavdangeti <abhinav@couchbase.com>
2017-11-09 12:14:56 -0800

    Adding encode/decode times to the tests

--- deltaComp_test.go
+++ deltaComp_test.go
@@ -8,6 +8,7 @@ package reductor
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func checkEq(a, b []uint32) bool {
@@ -34,13 +35,22 @@ func checkEq(a, b []uint32) bool {
 
 func testBasic(t *testing.T, postings []uint32) {
 	dcp := NewDeltaCompPostings()
+
+	start := time.Now()
 	dcp.AddAll(postings)
+	encodeTime := time.Since(start)
 
+	start = time.Now()
 	got := dcp.FetchAll()
+	decodeTime := time.Since(start)
 
+	fmt.Println("======================== RESULTS ==========================")
+	fmt.Println("Encoding time: ", encodeTime)
 	fmt.Printf("Achieved a compression from %v bytes to %v bytes => %.4v%%\n",
 		len(postings)*4, dcp.Len(),
 		float64(len(postings)*4-dcp.Len())*100/float64(len(postings)*4))
+	fmt.Println("Decoding time: ", decodeTime)
+	fmt.Println("===========================================================")
 
 	if !checkEq(postings, got) {
 		t.Errorf("Expected: %v, Got: %v", postings, got)
