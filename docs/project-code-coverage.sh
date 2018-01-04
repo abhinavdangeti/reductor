431ab9af1170c0effe439cb5da86cbb4938eaecc
abhinavdangeti <abhinav@couchbase.com>
2018-01-03 17:24:52 -0800

    Updates for coveralls

--- docs/project-code-coverage.sh
+++ docs/project-code-coverage.sh
@@ -1,39 +1,4 @@
 #!/bin/bash
 
-# Originally from bleve/docs/project-code-coverage.sh
-
-echo "mode: count" > acc.out
-for Dir in . $(find ./* -maxdepth 10 -type d | grep -v vendor);
-do
-    if ls $Dir/*.go &> /dev/null;
-    then
-        returnval=`go test -coverprofile=profile.out -covermode=count $Dir`
-        echo ${returnval}
-        if [[ ${returnval} != *FAIL* ]]
-        then
-            if [ -f profile.out ]
-            then
-                cat profile.out | grep -v "mode: count" >> acc.out
-            fi
-        else
-            exit 1
-        fi
-    fi
-done
-
-cat acc.out | go run docs/merge-coverprofile.go > merged.out
-
-if [ -n "$COVERALLS" ]
-then
-    export GIT_BRANCH=$TRAVIS_BRANCH
-    $HOME/gopath/bin/goveralls -service=travis-ci -coverprofile=merged.out
-fi
-
-if [ -n "$COVERHTML" ]
-then
-    go tool cover -html=merged.out
-fi
-
-rm -rf ./acc.out
-rm -rf ./profile.out
-rm -rf ./merged.out
+go test -v -covermode=count -coverprofile=coverage.out
+$HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
